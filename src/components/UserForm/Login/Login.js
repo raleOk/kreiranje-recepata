14affbf932b454269b7b7411d7d7aad1a9b9bc23
Rastko Zivkovic <rale.pablito@gmail.com>
2021-12-10 16:13:39 +0100

    set up a local json database with some users, fetching data from it

--- src/components/UserForm/Login/Login.js
+++ src/components/UserForm/Login/Login.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Login = props => {
   const [userData, setUserData] = useState({});
+  const [jsonData, setJsonData] = useState([]);
 
   const setUserDataHandler = event => {
     setUserData(prevState => {
@@ -9,10 +10,21 @@ const Login = props => {
     });
   };
 
+  useEffect(() => {
+    fetch("http://localhost:8000/users")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setJsonData(data);
+      });
+  }, []);
+
   const onSubmit = () => {
     console.log(userData);
-    props.logIn();
+    console.log(jsonData);
   };
+
   return (
     <div>
       <form>
