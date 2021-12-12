a57e0cab33107f5d72be81809c884afab1c2be82
Rastko Zivkovic <rale.pablito@gmail.com>
2021-12-12 23:58:50 +0100

    added a separate file for the fetch call configuration

---
+++ src/fetchConfig/fetchConfig.js
@@ -0,0 +1,11 @@
+const fetchConfig = {
+  url: "http://localhost:8000/users",
+  method: "POST",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+  body: null,
+};
+
+export default fetchConfig;
