4363bc5983d456ced8e215dc06a48a1dcfb4ba7d
Rastko Zivkovic <rale.pablito@gmail.com>
2021-12-12 23:59:43 +0100

    added a custom hook for making fetch calls

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,21 @@
+const useFetch = (fetchConfig, applyData) => {
+  const sendRequest = async () => {
+    try {
+      const response = await fetch(fetchConfig.url, {
+        method: fetchConfig.method ? fetchConfig.method : "GET",
+        headers: fetchConfig.headers ? fetchConfig.headers : {},
+        body: fetchConfig.body ? JSON.stringify(fetchConfig.body) : null,
+      });
+
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return {
+    sendRequest,
+  };
+};
+
+export default useFetch;
